51457b343eac8982c1b514237d17a9084a546c66
halfalo <113610878+halfalo@users.noreply.github.com>
2022-11-22 17:15:39 +0100

    HTML rész feltöltése az adduser.php-ba

--- Code/adduser.php
+++ Code/adduser.php
@@ -44,4 +44,48 @@ if(isset($_POST['adduser'])){
     }
 }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Regisztráció</title>
+    <link rel="stylesheet"  type="text/css">
+    </head>
+        <body>
+
+            <div class=>
+                <h1 class=>Regisztráció</h1>
+                <form action="adduser.php" method="POST" enctype="multipart/form-data">
+                    <div class="test">
+                    <label>Felhasználónév : </label>
+                    <br>
+                    <input type="text" name="username" placeholder="Írja be a felhasználónevet" id="reg" required>
+                    <br><br>
+                    <label>E-mail cím : </label>
+                    <br>
+                    <input type="email" name="email" placeholder="Írja be az e-mail címet" id="reg" required/>
+                    <br><br>
+                    <label>Jelszó : </label>
+                    <br>
+                    <input type="password" name="password" placeholder="Írja be a jelszót" id="reg" required/>
+                    <br><br>
+                    <label>Jelszó megerősítése : </label>
+                    <br>
+                    <input type="password" name="password_2" placeholder="Írja be a jelszót újra" id="reg" required/>
+                    <br><br>
+                    <label>Profilkép : </label>
+                    <br>
+                    <input type="file" name="image" id="img" required/>
+                    <br><br>
+                    <div class="g-recaptcha" data-sitekey="6LdIKcMaAAAAADmvhs5b3A9tce1GdjovsfLWvU8M"></div>
+                    <br>
+                    <button type="submit" name="register" class="reglog">Regisztráció</button>
+                    <br><br>
+                    </div>
+                 </form>
+            </div>
+        </body>
+
+
+</html>
 
