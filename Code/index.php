e44bd7bc5ccbae009d3b04b24f8c9c94c257047a
Saviur <113610538+103470@users.noreply.github.com>
2022-11-28 19:37:20 +0100

    index.php kérdések megjelenítése

--- Code/index.php
+++ Code/index.php
@@ -1,3 +1,16 @@
 <?php
 
 require 'connect.php'; ?>
+
+<?php
+$number = $_GET['n'];
+$qidresult = $con->query("SELECT qid FROM questions");
+$qid = 1;
+while($row = mysqli_fetch_assoc($qidresult)){
+    $qid = $row['qid'];
+}
+$qresult = $con->query("SELECT * FROM questions WHERE sn = $number");
+$question = mysqli_fetch_assoc($qresult);
+$choices = $con->query("SELECT * FROM answers WHERE qid = $qid");
+
+?>
