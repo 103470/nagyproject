6434776834429d0c73946fc7991159a745c67057
Saviur <113610538+103470@users.noreply.github.com>
2022-11-28 19:34:07 +0100

    addchoice.php - kérdés + válasz hozzáadásának lehetősége
    Kérdés hozzáadása majd a hozzátartozóü válaszok megadása, amely az adatbázisban került eltárolásra

--- Code/addchoice.php
+++ Code/addchoice.php
@@ -2,3 +2,38 @@
 
 require 'connect.php'; ?>
 
+<?php
+    if(isset($_POST['addqc'])){
+        $qid = rand(1,100);
+        $result = $con->query("SELECT * FROM questions WHERE qid = $qid");
+        while(mysqli_num_rows($result) > 0){
+            ++$qid;
+        }
+        $sn = $_POST['number'];
+        $question = $_POST['question'];
+        $choicenum = 4;
+        $con->query("INSERT INTO questions (qid, questions, choice, sn) VALUES ('$qid', '$question', '$choicenum', '$sn')");
+        
+        $choice = array();
+        $choice[1] = $_POST['choice1'];
+        $choice[2] = $_POST['choice2'];
+        $choice[3] = $_POST['choice3'];
+        $choice[4] = $_POST['choice4'];
+
+        $count = count($choice);
+        for($i=1; $i <= $count; $i++){
+
+        $con->query("INSERT INTO answers (qid, answer) VALUES ('$qid', '$choice[$i]')");
+        }
+
+
+    }
+
+    $query = "SELECT * FROM questions";
+		$questions = mysqli_query($con,$query);
+		$total = mysqli_num_rows($questions);
+		$next = $total + 1;
+		
+
+
+?>
