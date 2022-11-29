b007986bee483386c3f82519d8387d606b087ccf
Saviur <113610538+103470@users.noreply.github.com>
2022-11-29 17:17:49 +0100

    processp.php - predikció + elmentése az adatbázisba funkció

--- Code/processp.php
+++ Code/processp.php
@@ -1,1 +1,31 @@
+<?php 
+    require 'connect.php';?>
 
+
+<?php
+if(isset($_POST['next'])){
+        $number = $_POST['number'];
+        $question = $_POST['question'];
+        $answer = $_POST['answer'];
+        $con->query("INSERT INTO userprediction (question, answer) VALUES ('$question', '$answer')");
+        
+
+        $dbresult = $con->query("SELECT count(id) AS total FROM prediction"); 
+        $num_rows = mysqli_fetch_assoc($dbresult);
+        $total_question = $num_rows['total'];
+
+
+        $next = $number + 1;
+
+
+        if($number == $total_question){
+            header("location: final.php");
+        }
+        else{
+            header("location: prediction.php?n=". $next);
+        }
+    
+
+}
+
+?>
