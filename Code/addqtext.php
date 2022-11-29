56ef32aa3cfb1751a5bb0a30c914d59d300c75e9
Saviur <113610538+103470@users.noreply.github.com>
2022-11-29 17:26:39 +0100

    addqtext.php - Predikciót váró kérdés hozzáadása

--- Code/addqtext.php
+++ Code/addqtext.php
@@ -1,1 +1,18 @@
+<?php
 
+require 'connect.php'; ?>
+
+<?php 
+if(isset($_POST['addqt'])){
+    $number = $_POST['number'];
+    $question = $_POST['question'];
+    $con->query("INSERT INTO prediction (question, sn) VALUES ('$question', '$number')");
+}
+
+$query = "SELECT * FROM prediction";
+$questions = mysqli_query($con,$query);
+$total = mysqli_num_rows($questions);
+$next = $total + 1;
+		
+    
+?>
