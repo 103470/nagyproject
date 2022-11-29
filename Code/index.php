6bdeba6bce02aa5a376e57da01ccae41a20bd1a1
halfalo <113610878+halfalo@users.noreply.github.com>
2022-11-29 23:45:22 +0100

    Index.php HTML kód javítása

--- Code/index.php
+++ Code/index.php
@@ -14,24 +14,4 @@ $question = mysqli_fetch_assoc($qresult);
 $choices = $con->query("SELECT * FROM answers WHERE qid = $qid");
 
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Kérdőív</title>
-    </head>
-    <body>
-    <form method="POST" action="process.php" >
-        <label><?php echo $question['questions']; ?> </label>
-            <ul>
-                <?php while($row = mysqli_fetch_array($choices)) { ?>
-                <input type="radio" value="<?php echo $row['answer']; ?>"><?php echo $row['answer']; ?>
-                <?php } ?>
-            <br>
-            </ul>
 
-            <input type="hidden" name="number" value="<?php echo $number; ?>">
-            <button name="next" value="next">Tovább</button>
-        </form>
-    </body>
-</html>
