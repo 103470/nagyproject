a7297d2a9ddc6b841ff01e11e1aae6df54067a07
Saviur <113610538+103470@users.noreply.github.com>
2022-11-29 19:38:11 +0100

    database.php - Kérdés + válasz megjelenítése a képernyőn

--- Code/database.php
+++ Code/database.php
@@ -1,1 +1,20 @@
+<?php
+    require 'connect.php'; ?>
 
+<?php
+    $result = $con->query("SELECT * FROM useranswer");
+    $num_rows = mysqli_num_rows($result);
+
+    
+    
+    $result2 = $con->query("SELECT * FROM userprediction");
+    $num_rows2 = mysqli_num_rows($result2);
+
+  
+
+    
+
+    
+
+
+?>
