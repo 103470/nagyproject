7d54c52e0b8e3864f50d4db688997ea751f6da40
Saviur <113610538+103470@users.noreply.github.com>
2022-11-29 19:36:36 +0100

    index.php - 7. sor javítása
    $qidresult = $con->query("SELECT qid FROM questions WHERE sn = $number");
    sn = $number hozzáadása

--- Code/index.php
+++ Code/index.php
@@ -4,7 +4,7 @@ require 'connect.php'; ?>
 
 <?php
 $number = $_GET['n'];
-$qidresult = $con->query("SELECT qid FROM questions");
+$qidresult = $con->query("SELECT qid FROM questions WHERE sn = $number");
 $qid = 1;
 while($row = mysqli_fetch_assoc($qidresult)){
     $qid = $row['qid'];
