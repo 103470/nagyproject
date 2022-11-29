21265ffc56e7bb6d53d7a6f74feefdd266897e24
halfalo <113610878+halfalo@users.noreply.github.com>
2022-11-29 23:37:30 +0100

    HTML váz hozzáadása a final.php-hez

--- Code/final.php
+++ Code/final.php
@@ -1,1 +1,13 @@
+<html>
+<head>
+    <title>Kérdőív</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
 
+    <header>
+        <ul>
+            <li><a href="main.php">Kezdőlap</a></li>
+            <li><a href="login.php">Bejelentkezés</a></li>
+        </ul>
+    </header>
