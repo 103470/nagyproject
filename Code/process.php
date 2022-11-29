f6326ad94916c2f006573a46e90b8a16bbf30f51
Saviur <113610538+103470@users.noreply.github.com>
2022-11-29 16:43:22 +0100

    process.php - kérdőív léptetéséért felelő kód megírása

--- Code/process.php
+++ Code/process.php
@@ -1,1 +1,29 @@
+<?php
 
+require 'connect.php'; 
+
+?>
+<?php 
+    if(isset($_POST['next'])){
+        $number = $_POST['number'];
+        
+
+        $dbresult = $con->query("SELECT count(id) AS total FROM questions"); 
+        $num_rows = mysqli_fetch_assoc($dbresult);
+        $total_question = $num_rows['total'];
+
+
+        $next = $number + 1;
+
+
+        if($number == $total_question){
+            header("location: final.php");
+        }
+        else{
+            header("location: index.php?n=". $next);
+        }
+    
+
+}
+    
+?>
