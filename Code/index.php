c33eee3865384ff12f159d0592b9d53fae846c13
halfalo <113610878+halfalo@users.noreply.github.com>
2022-11-29 23:45:58 +0100

    Válaszok megjelenítésének javítása az index.php ban

--- Code/index.php
+++ Code/index.php
@@ -14,4 +14,32 @@ $question = mysqli_fetch_assoc($qresult);
 $choices = $con->query("SELECT * FROM answers WHERE qid = $qid");
 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Kérdőív</title>
+    </head>
+    <body>
+    <div class="index">
+    <form method="POST" action="process.php" >
+        <h3 class="h2index"><label><?php echo $question['questions']; ?> </label></h3>
+        <input type="hidden" name="question" value="<?php echo $question['questions']; ?> ">
+            <br>
+            <ul>
+                <?php while($row = mysqli_fetch_array($choices)) { ?>
+                <input type="radio" name ="answer" value="<?php echo $row['answer']; ?>" required ><?php echo $row['answer']; ?>
+                <?php } ?>
+            <br>
+            </ul>
+            <br>
+
+            <input type="hidden" name="number" value="<?php echo $number; ?>"> 
+            <button name="next" value="next">Tovább</button>
+            <br>
+        </form>
+    </div>
+    </body>
+</html>
 
