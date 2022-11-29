5f9ec3a33cf0e8dcfa8a6173265f741622593879
Saviur <113610538+103470@users.noreply.github.com>
2022-11-29 17:19:24 +0100

    prediction.php - predikció kérő kérdések megjelenítése

--- Code/prediction.php
+++ Code/prediction.php
@@ -1,1 +1,13 @@
+<?php 
+    require 'connect.php';?>
 
+<?php
+    $number = $_GET['n'];
+    $qresult = $con->query("SELECT * FROM prediction WHERE sn = $number");
+    $question = mysqli_fetch_assoc($qresult);
+
+
+
+
+    
+?>
