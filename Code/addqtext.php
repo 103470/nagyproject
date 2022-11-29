f1aa8d0edbf9175b297f609b12d92b0f1fc353f3
halfalo <113610878+halfalo@users.noreply.github.com>
2022-11-29 23:32:02 +0100

    input type javítása az aaddqtext-ben

--- Code/addqtext.php
+++ Code/addqtext.php
@@ -16,3 +16,37 @@ $next = $total + 1;
 		
     
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kérdőív szerkesztése</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <header>
+        <ul>
+            <li><a href="addchoice.php">Kérdés hozzáadása</a></li>
+            <li><a href="addqtext.php">Predikció hozzáadása</a></li>
+            <li><a href="logout.php">Kijelentkezés</a></li>
+        </ul>
+    </header>
+    <main>
+    <div class="nagytest">
+    <form method="POST" action="addqtext.php">
+        <div class="test">
+            <br>
+                <label>Kérdés száma</label>
+                <input type="number" name="number" class="szam" value="<?php echo $next;  ?>">
+            <br> 
+            <br>
+                <label>Kérdés :</label>
+                <input type="text" name="question" class="textbox">
+            <br>
+
+            <button name="addqt" value="addqt">Hozzáadás</button>
+        </div>
+    </form>
+    </div>
+
+    </main>
+    </body>
