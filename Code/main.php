4920ab41d78525242edde8a21c7ef2052509fe2f
halfalo <113610878+halfalo@users.noreply.github.com>
2022-11-29 23:49:09 +0100

    HTML kód hozzáadása a main.php hez

--- Code/main.php
+++ Code/main.php
@@ -5,3 +5,38 @@ $total = mysqli_num_rows($result);
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kezdőlap</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <header>
+        <ul>
+            <li><a href="main.php">Kezdőlap</a></li>
+            <li><a href="login.php">Bejelentkezés</a></li>
+        </ul>
+
+    </header>
+
+    <main>
+            <div class="main">
+                <h2>Mennyire vagy jártas a foci világában? </h2>
+                <br>
+                    Egy felelet választós kérdéssorozatban tesztelheted tudásod.
+                <br>
+                <ul>
+                    <li><strong>Kérdések száma:</strong><?php echo $total; ?> </li>
+                    <li><strong>Típus: </strong>Feleletválasztós</li>
+
+                </ul>
+
+                <li><a href="index.php?n=1" class="start">Kitöltés</a><li>
+
+            </div>
+
+    </main>
+</body>
+</html>
