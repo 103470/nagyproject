8ed877f0866d9c0a94d1b0270cffa1435d8d03cf
halfalo <113610878+halfalo@users.noreply.github.com>
2022-11-29 23:50:34 +0100

    HTML kód hozzáadása a prediction.php-hez

--- Code/prediction.php
+++ Code/prediction.php
@@ -11,3 +11,26 @@
 
     
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Kérdőív</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <div class="index">
+    <form method="POST" action="processp.php" >
+        <h3><label><?php echo $question['question']; ?> </label></h3>
+        <input type="hidden" name="question" class="textbox" value="<?php echo $question['question']; ?> ">
+            <ul>
+                <input type="text" name="answer" class="textbox" required>
+            <br>
+            </ul>
+
+            <input type="hidden" name="number" value="<?php echo $number; ?>">
+            <button name="next" value="next">Tovább</button>
+        </form>
+        </div>
+    </body>
+</html>
